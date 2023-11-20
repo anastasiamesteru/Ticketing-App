ad4ddd371cb24c0ae0f928ea9b2eaf5fa4352ce5
Anastasia Mesteru <mesteruanastasia22@stud.ase.ro>
2023-11-20 16:52:36 +0200

    date setters and getters

---
+++ Project/Project/date.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum class month
+{
+	JANUARY = 1,
+	FEBRUARY,
+	MARCH,
+	APRIL,
+	MAY,
+	JUNE,
+	JULY,
+	AUGUST,
+	SEPTEMBER,
+	OCTOBER,
+	NOVEMBER,
+	DECEMBER
+};
+
+class Date
+{
+private:
+
+	unsigned int minutes = 0;
+	unsigned int hour = 0;
+	//month month = JANUARY;??
+	unsigned int day = 0;
+	unsigned int year = 2010;
+
+	static const int MAX_YEAR = 2050;
+
+	// Setters
+
+	void setMinutes(unsigned int minutes);
+	void setHour(unsigned int hour);
+	void setMonth(month month);
+	void setYear(unsigned int year);
+	void setDay(unsigned int day);
+
+
+public:
+
+	// Getters
+
+	unsigned int getMinutes();
+	unsigned int getHour();
+	month getMonth();
+	unsigned int getYear();
+	unsigned int getDay();
+
+};
